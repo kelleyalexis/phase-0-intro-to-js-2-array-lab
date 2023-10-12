b685ee4be967f27c2101305bcf7d9d3593f5f286
Kelley Shackelford <kelleyalexis@yahoo.com>
2023-10-12 14:17:11 -0400

    defined array functions

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield"
+]
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+function destructivelyRemoveFirstCat(name) {
+  cats.shift(name);
+}
+function destructivelyRemoveLastCat(name) {
+  cats.pop(name);
+}
+function appendCat(name) {
+  const copyOfCats = cats.concat(name);
+  return copyOfCats;
+}
+function prependCat(name) {
+  const copyOfCats = cats.slice();
+  copyOfCats.unshift(name); 
+  return copyOfCats; 
+}
+function removeLastCat(name) {
+  const copyOfCats = cats.slice(0, cats.length - 1); 
+  return copyOfCats; 
+}
+function removeFirstCat(name) {
+  const copyOfCats = cats.slice(1); 
+  return copyOfCats; 
+}
